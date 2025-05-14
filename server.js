17c2781b5b2baf9a66004ab15311e1ed4a78ca5c
meilecor <corinne.meile@gmail.com>
2025-05-15 01:49:28 +0200

    Neuer Test für GitHub Actions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Woche 13: Docker Deployment für Azure');
+  res.send('Hello FS2025 DevOps Course! Woche 13: Docker Deployment für Azure mit GitHub Actions');
 });
 
 app.listen(PORT, HOST);
